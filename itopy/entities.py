1185a315a4591cb96ee2d89b3172fbfdf22339d2
Andrea Carmisciano <andrea.carmisciano@gmail.com>
2018-11-27 15:40:15 +0100

    cleaner code

--- itopy/entities.py
+++ itopy/entities.py
@@ -1,5 +1,6 @@
 import json
 from collections import namedtuple
+import datetime
 
 
 class BaseEntity(object):
@@ -14,6 +15,25 @@ class BaseEntity(object):
     def find(self, where):
         return self.client.get(OBJECT_CLASS, "SELECT {} WHERE {}".format(OBJECT_CLASS, where))
 
+    @property
+    def select(self):
+        if not hasattr(self, 'klass'):
+            raise ValueError("query require valid instance")
+
+        self.current_query = "SELECT {} ".format(self.klass)
+
+    def select_where(self, clause):
+        if not hasattr(self, 'klass'):
+            raise ValueError("query require valid instance")
+
+        self.current_query = "SELECT {} WHERE {} ".format(self.klass, clause)
+
+    def where(self, clause):
+        if "WHERE" in self.current_query:
+            self.current_query += " AND {}".format(clause)
+        else:
+            self.current_query += " WHERE {}".format(clause)
+
     def find_by_name(self, name, active=True):
         if not hasattr(self, 'klass'):
             raise ValueError("find_by_name cannot be called on {} object".format(BaseEntity.OBJECT_CLASS))
@@ -27,22 +47,21 @@ class BaseEntity(object):
         return self.deserialize(data)
 
     def find_by_id(self, id, active=True):
-        if not hasattr(self, 'klass'):
-            raise ValueError("find_by_name cannot be called on {} object".format(BaseEntity.OBJECT_CLASS))
-
-        query = "SELECT {} WHERE id = '{}'".format(self.klass, id)
+        self.select_where("id = '{}'".format(id))    
         if not active:
-            query += " AND status = 0 "
-
-        data = self.client.get(self.klass, query)
-
-        return self.deserialize(data)
+            self.where("status = 0")
+        
+        return self.fetch()
 
     def delete(self):
         raise NotImplementedError
 
     def update(self):
         raise NotImplementedError
+
+    def fetch(self):
+        data = self.client.get(self.klass, self.current_query)
+        return self.deserialize(data)
 
     def deserialize(self, data):
         return Utils.deserialize(self.klass, data, self.client)
@@ -71,44 +90,46 @@ class UserRequest(BaseEntity):
     def __init__(self, client):
         super(UserRequest, self).__init__(client)
         self.klass = UserRequest.OBJECT_CLASS
+        self.current_query = None
 
     def find_by_organization_id(self, id, active=True, last_update=None):
-        if not hasattr(self, 'klass'):
-            raise ValueError("find_by_organization_id cannot be called on {} object".format(BaseEntity.OBJECT_CLASS))
 
-        query = "SELECT {} WHERE org_id = '{}'".format(self.klass, int(id))
-        if not active:
-            query += " AND status IN ('rejected', 'resolved', 'closed')"
-        else:
-            query += " AND status NOT IN ('rejected', 'resolved', 'closed')"
+        self.select_where("org_id = '{}'".format(id))
+        self.where(self.query_active) if active else self.where(self.query_inactive)
+        
+        if last_update:
+            self.where("last_update >= '{:%Y-%m-%d %H:%M:%S}'".format(last_update))
 
-        data = self.client.get(self.klass, query)
+        return self.fetch()
 
-        return self.deserialize(data)
+    def find_by_team(self, name, org_id, active=True, last_update=None):
 
-    def find_by_assignee(self, name, org_id=None, active=True, last_update=None):
-        if not hasattr(self, 'klass'):
-            raise ValueError("find_by_assegnee cannot be called on {} object".format(BaseEntity.OBJECT_CLASS))
-
-        query = "SELECT {} WHERE agent_id_friendlyname = '{}'".format(self.klass, name)
-        if not active:
-            query += " AND status IN ('rejected', 'resolved', 'closed') "
-        else:
-            query += " AND status NOT IN ('rejected', 'resolved', 'closed') "
-
-        if org_id:
-            query += " AND org_id = '{}'".format(org_id)
+        self.select_where("team_id_friendlyname = '{}' AND org_id = {}".format(name, org_id))
+        self.where(self.query_active) if active else self.where(self.query_inactive)
 
         if last_update:
-            assert type(last_update) is datetime
-            query += " AND last_update >= {}".format(last_update)
+            self.where("last_update >= '{:%Y-%m-%d %H:%M:%S}'".format(last_update))
 
-        data = self.client.get(self.klass, query)
+        return self.fetch()
 
-        return self.deserialize(data)
 
+    def find_by_assignee(self, name, org_id, active=True, last_update=None):
+
+        self.select_where("agent_id_friendlyname = '{}' AND org_id = {}".format(name, org_id))
+        self.where(self.query_active) if active else self.where(self.query_inactive)
+
+        if last_update:
+            self.where("last_update >= '{:%Y-%m-%d %H:%M:%S}'".format(last_update))
+
+        return self.fetch()
         
-        
+    @property
+    def query_inactive(self):
+        return "status IN ('rejected', 'resolved', 'closed') "
+
+    @property
+    def query_active(self):
+        return "status NOT IN ('rejected', 'resolved', 'closed') "
 
 class Utils:
 
